7659198b1294c39e1a29ef0e7e64792a7a27a8d4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-05 08:44:20 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "dbdb2848-729b-4352-aa22-59ddd298754c",
+    "home_feed": [
+        {
+            "id": "87976a0a-e6a6-4b9c-8aa0-b31c27755e99",
+            "artist_name": "about",
+            "home_carousel": [
+                {
+                    "id": "17de51ee-e308-4bb7-b5d6-c2d1dfd0b1f6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "d01a4c56-e7cc-4d7d-ba84-e14bace4041d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "41559423-563e-4a69-85b0-33d4ab069e7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/143.png"
+                },
+                {
+                    "id": "37ea7c42-3866-4079-ae2f-ba6d8f297871",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "6be5bf2f-44ec-48cf-97d0-5fc2c22902c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                }
+            ],
+            "art_caption": "between",
+            "art_description": "Stand civil player music important agent. Tough bed fund pick local decade four."
+        },
+        {
+            "id": "7424a5a4-3412-4f77-a73c-fa56a0fc5cab",
+            "artist_name": "little",
+            "home_carousel": [
+                {
+                    "id": "9443c4d3-0132-4da5-b8ec-ee1b66d9e21c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "91efbf18-a7f2-4ada-8c7f-282ab7e3d2a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "8907fe53-2711-4692-9088-b608e4a83ebe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "259c98a7-70e8-4828-a19c-76af2040f5d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "c63060ec-f79a-4440-9d48-0eb6e611f4f9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                }
+            ],
+            "art_caption": "wife",
+            "art_description": "Study level per consumer. Respond matter final benefit law attack way."
+        },
+        {
+            "id": "257031ba-a642-424f-8b01-aa2361cc84c6",
+            "artist_name": "eye",
+            "home_carousel": [
+                {
+                    "id": "5c96f54a-21e0-49a8-bfe0-78c0058014b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "f8fb22a4-5136-44bb-8dc7-e1490b97c335",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "cc3dd2b2-30a8-4e24-aab1-9479e3a64522",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "1627b936-b829-4753-aa0d-1fbed93ac782",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "b72ba137-c593-44aa-abda-5ad2878d5dbb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                }
+            ],
+            "art_caption": "series",
+            "art_description": "Knowledge second in want economy off task. Road card exist provide. Teacher college war field you prove deal."
+        },
+        {
+            "id": "7fa8a265-6e03-40ff-bb29-c2f7182aff45",
+            "artist_name": "course",
+            "home_carousel": [
+                {
+                    "id": "ae1dc325-121e-4f05-a28f-cc0619577098",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "f60c515e-f439-436b-a5c7-7f990522aaf9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png"
+                },
+                {
+                    "id": "5cda812e-224b-4eb2-9a6c-ba0c1550cbe5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "8550cee9-bc0f-4ad8-8e68-b5451c43c5d6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+                },
+                {
+                    "id": "9765d22c-bacc-4bc1-bd6a-49eaa3638992",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                }
+            ],
+            "art_caption": "foreign",
+            "art_description": "Attention body TV green prevent. Successful represent shoulder table hour up usually. Foot result there watch."
+        },
+        {
+            "id": "120607ab-8673-4534-9447-31d639050cc3",
+            "artist_name": "beautiful",
+            "home_carousel": [
+                {
+                    "id": "0acab68e-a515-45c8-80d0-7587b06888a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "f364cb5d-fb35-4337-9255-497877559d37",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "b4e81685-c9a5-44f6-a86d-030466bbde48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png"
+                },
+                {
+                    "id": "ffe837ec-53eb-48a9-80e3-46c6f296be9c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "405581a4-91b8-47dc-b22b-0de80457760d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                }
+            ],
+            "art_caption": "style",
+            "art_description": "Often first local."
+        }
+    ]
+}
